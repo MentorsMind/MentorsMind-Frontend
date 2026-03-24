c7b88def232d8895a254ad90e795a6575fedc551
amochuko <7204868+amochuko@users.noreply.github.com>
2026-03-24 12:51:38 +0100

    refactor with useQuery

--- src/hooks/useChartData.ts
+++ src/hooks/useChartData.ts
@@ -7,8 +7,15 @@ import type {
 export function useChartData<T>({
   fetchFn,
   deps = [],
+  staleTime = 1000 * 60, // 1 min default
+  gcTime = 1000 * 60 * 5, // 5 min default
 }: UseChartDataOptions<T>): UseChartDataResult<T> {
-  const q = useQuery<T>({ queryKey: ["chartData", ...deps], queryFn: fetchFn });
+  const q = useQuery<T>({
+    queryKey: ["chartData", ...deps],
+    queryFn: fetchFn,
+    staleTime,
+    gcTime,
+  });
 
   return {
     data: q.data ?? null,
