66bf25f0d6f4d50e2b50cfb179e8455d3db924f6
amochuko <7204868+amochuko@users.noreply.github.com>
2026-03-25 10:37:50 +0100

    update with args options

--- src/services/mentor.service.ts
+++ src/services/mentor.service.ts
@@ -1,37 +1,37 @@
 import { apiConfig } from "../config/api.config";
+import type { RequestOptions } from "../types/api.types";
 import { request } from "../utils/request.utils";
 
 export default class MentorService {
-  async getMentors() {
+  async getMentors(opts?: RequestOptions) {
     return request<{ id: string; name: string }[]>(
       {
         method: "GET",
         url: apiConfig.url.mentors,
       },
-      {
-        useCache: true,
-      },
+      opts,
     );
   }
 
-  async getMentor(id: string) {
+  async getMentor(id: string, opts?: RequestOptions) {
     return request<{ id: string; name: string }[]>(
       {
         method: "GET",
         url: apiConfig.url.mentors,
         data: { id },
       },
-      {
-        useCache: true,
-      },
+      opts,
     );
   }
 
-  async add(args: Record<string, unknown>) {
-    return request<{ id: string; name: string }[]>({
-      method: "POST",
-      url: apiConfig.url.mentors,
-      data: { args },
-    });
+  async add(args: Record<string, unknown>, opts?: RequestOptions) {
+    return request<{ id: string; name: string }[]>(
+      {
+        method: "POST",
+        url: apiConfig.url.mentors,
+        data: { args },
+      },
+      opts,
+    );
   }
 }
--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -1,38 +1,38 @@
 import { apiConfig } from "../config/api.config";
+import type { RequestOptions } from "../types/api.types";
 import { request } from "../utils/request.utils";
 
 export default class SessionService {
-  async create(mentorId: string) {
-    return request<{ status: string }>({
-      method: "POST",
-      url: apiConfig.url.sessions,
-      data: { mentorId },
-    });
+  async create(mentorId: string, opts?: RequestOptions) {
+    return request<{ status: string }>(
+      {
+        method: "POST",
+        url: apiConfig.url.sessions,
+        data: { mentorId },
+      },
+      opts,
+    );
   }
 
-  async getSession(id: string) {
+  async getSession(id: string, opts?: RequestOptions) {
     return request<{ status: string }>(
       {
         method: "GET",
         url: apiConfig.url.sessions,
         data: { id },
       },
-      {
-        useCache: true,
-      },
+      opts,
     );
   }
 
-  async getSessions(args: Record<string, unknown>) {
+  async getSessions(args: Record<string, unknown>, opts?: RequestOptions) {
     return request<{ status: string }>(
       {
         method: "GET",
         url: apiConfig.url.sessions,
         data: { args },
       },
-      {
-        useCache: true,
-      },
+      opts,
     );
   }
 }
