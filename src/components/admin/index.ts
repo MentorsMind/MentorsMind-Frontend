63fe7ed6b913c95be438c7a6940d4dedb8c000b0
AbuJulaybeeb <abujulaybeeb08@gmail.com>
2026-04-25 10:30:45 +0100

    feat: implement sandboxed email template preview for admin panel

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -5,4 +5,5 @@ export { default as AdminSessions } from './AdminSessions';
 export { default as AdminPayments } from './AdminPayments';
 export { default as AdminDisputes } from './AdminDisputes';
 export { default as AdminLogs } from './AdminLogs';
+export { default as EmailTemplatePreview } from './EmailTemplatePreview';
 export { AdminTable } from './AdminTable';
